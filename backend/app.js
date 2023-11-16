bfdfbcefe8b7abd4ab489aff2ea9dbd2a121bf47
ywj128 <wanjingy128@gmail.com>
2023-11-16 15:42:14 -0800

    Set up MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes")
-const HttpError = require("./models/http-error")
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-    const error = new HttpError('Could not find this router.', 404);
-    throw error;
+  const error = new HttpError("Could not find this router.", 404);
+  throw error;
 });
 
 app.use((err, req, res, next) => {
@@ -25,4 +26,7 @@ app.use((err, req, res, next) => {
   res.json({ message: err.message || "An unknown error occured!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect("mongodb+srv://wanjing:zAUCrzuTkNDbHH82@atlascluster.h407xs5.mongodb.net/places?retryWrites=true&w=majority")
+  .then(() => app.listen(5000))
+  .catch((err) => console.log(err));
