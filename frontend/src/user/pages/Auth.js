789da6e7dd1f0680503f95c74eedd54e93c0405a
ywj128 <wanjingy128@gmail.com>
2023-11-27 16:57:32 -0800

    Add a ImageUpload component

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -37,6 +38,7 @@ const Auth = () => {
         {
           ...formState.inputs,
           name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -48,6 +50,10 @@ const Auth = () => {
             value: "",
             isValid: false,
           },
+          image:{
+            value: null,
+            isValid: false
+          }
         },
         false
       );
@@ -57,6 +63,9 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+
+    console.log(formState.inputs);
+
     try {
       let responseData;
       if (isLoginMode) {
@@ -108,6 +117,7 @@ const Auth = () => {
               onInput={inputHandler}
             ></Input>
           )}
+          {!isLoginMode && <ImageUpload id="image" center onInput={inputHandler}/>}
           <Input
             id="email"
             element="input"
