b0a71c6d1594f01e16f83a5e9473dcfcfc777bad
ywj128 <wanjingy128@gmail.com>
2023-11-27 23:17:25 -0800

    Fix auth bug

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -69,28 +69,27 @@ const Auth = () => {
     try {
       let responseData;
       if (isLoginMode) {
-        const formData = new FormData();
-        formData.append("email", formState.inputs.email.value);
-        formData.append("name", formState.inputs.name.value);
-        formData.append("password", formState.inputs.password.value);
-        formData.append("image", formState.inputs.image.value);
         responseData = await sendRequest(
           "http://localhost:5000/api/users/login",
           "POST",
-          formData
-        );
-      } else {
-        responseData = await sendRequest(
-          "http://localhost:5000/api/users/signup",
-          "POST",
           JSON.stringify({
-            name: formState.inputs.name.value,
             email: formState.inputs.email.value,
             password: formState.inputs.password.value,
           }),
           {
             "Content-Type": "application/json",
           }
+        );
+      } else {
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
+        responseData = await sendRequest(
+          "http://localhost:5000/api/users/signup",
+          "POST",
+          formData
         );
       }
       auth.login(responseData.user.id);
