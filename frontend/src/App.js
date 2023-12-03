dabcc47f50035da9c523ddc726d553786af43a47
ywj128 <wanjingy128@gmail.com>
2023-12-03 01:54:31 -0800

    Add token expiration management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,14 +18,17 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setUserId(uid);
     setToken(token);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
     localStorage.setItem(
       "userData",
       JSON.stringify({
         userId: uid,
         token,
+        expiration: tokenExpirationDate.toISOString(),
       })
     );
   }, []);
@@ -36,13 +39,16 @@ const App = () => {
     localStorage.removeItem("userData");
   }, []);
 
-  
-  useEffect(()=>{
+  useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
-    if(storedData && storedData.token){
-      login(storedData.userId, storedData.token)
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
     }
-  }, [login])
+  }, [login]);
 
   let routes;
 
