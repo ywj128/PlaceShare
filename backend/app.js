2d401a82a5150d3584ac74b09200ab2579870ef7
ywj128 <wanjingy128@gmail.com>
2023-12-30 00:07:07 -0800

    Add env port

--- backend/app.js
+++ backend/app.js
@@ -47,5 +47,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@atlascluster.h407xs5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => console.log(err));
